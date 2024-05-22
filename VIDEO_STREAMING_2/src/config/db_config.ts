b8740af4b77a2f6329675fb78604de3aedbf6f8d
RaviKumar-Nemala <133774005+RaviKumar-Nemala@users.noreply.github.com>
2024-05-22 06:15:46 +0530

    Update db_config.ts

--- VIDEO_STREAMING_2/src/config/db_config.ts
+++ VIDEO_STREAMING_2/src/config/db_config.ts
@@ -2,11 +2,11 @@ import { DataSource}  from 'typeorm';
 import  path  from 'path';
 
 export let AppDataSource : DataSource = new DataSource({
-    host : process.env.DATABASE_HOST || 'localhost',
-    port : Number(process.env.DATABASE_PORT ) || 3306,
-    username : process.env.DATABASE_USERNAME|| 'root',
-    database : process.env.DATABASE_NAME || 'video_streaming_2',
-    password:  process.env.DATABASE_PASSWORD ||'thisandthatddosattacking',
+    host : 
+    port : 
+    username : 
+    database :
+    password:  
     type :'mysql',
     migrations : [path.resolve(__dirname, "../migration/**/*{.ts,.js}")],
     entities : [path.resolve(__dirname , '../entities/**/*{.ts,.js}')],
